e1bc915c12b45522eae6f916c7067de13fc1ff30
Ameya Chavan <68412996+ameyachavan91@users.noreply.github.com>
2023-01-30 13:41:46 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,1 +1,28 @@
-print("Hello World")
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import json
+
+# Create a Spark session
+spark = SparkSession.builder.appName("KafkaProducer").getOrCreate()
+
+# Define the schema for the data
+schema = StructType([
+    StructField("key", StringType(), True),
+    StructField("value", StringType(), True)
+])
+
+# Create a dataframe with sample data
+data = [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
+df = spark.createDataFrame(data, schema)
+
+# Convert the dataframe to a stream
+stream = df.writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("topic", "test") \
+    .option("checkpointLocation", "./checkpoints") \
+    .start()
+
+# Wait for the stream to finish
+stream.awaitTermination()
